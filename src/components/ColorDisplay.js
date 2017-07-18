84a5d5f7b3bc327e32db5a51671d08699a2dee67
Anne Kotecha <anne.kotecha@gmail.com>
2017-07-18 12:36:33 +0100

    Added initial StaggeredMotion effect from react-motion.

--- src/components/ColorDisplay.js
+++ src/components/ColorDisplay.js
@@ -6,19 +6,49 @@ App contains:
 */
 
 import React, { Component } from 'react';
+import {StaggeredMotion, Motion, spring, presets} from 'react-motion';
 
 class ColorDisplay extends Component{
 
   constructor(props) {
     super(props);
     this.state = {
-      swatches: [],
-      color: "#0161b2"
+      colors: [],
+      colorElements: [],
+      yOff: 0,
+       items: [
+        {text: 'j4q2q7lu'},
+        {text: 'j4q2q9rq'},
+        {text: 'j4q2qb7k'},
+        {text: 'j4q2q7lu'},
+        {text: 'j4q2q9rq'},
+        {text: 'j4q2qb7k'}
+      ],
+      colors2: [
+        {text: '#e7260d'},
+        {text: '#ac0000'},
+        {text: '#750000'},
+        {text: '#4d0000'},
+        {text: '#400000'}
+      ]
     }
 
     this.updateColors = this.updateColors.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.move = this.move.bind(this);
    
+  }
+
+  move(e) {
+    if(this.state.yOff>99) {
+      this.setState({
+        yOff: 0
+      })
+    } else {
+      this.setState({
+        yOff: 100
+      })
+    }
   }
 
   updateColors() {
@@ -31,41 +61,80 @@ class ColorDisplay extends Component{
     // Create new array of colours
     const colorList = [];
     for (var i = 0; i < steps; i++) {
-        colorList[i] = chromaColor.darken(i);
+        let color = chromaColor.darken(i).hex();
+        colorList.push({color:color});
+        //colorList[i] = chromaColor.darken(i);
     }
+    this.setState({colors: colorList});
+    console.log(colorList.toString())
 
     // Create a new element for each color
     const elements = []
     for (var j = 0; j < colorList.length; j++) {
-        const color = colorList[j];
+        const color = colorList[j].color;
+        console.log('color', color);
         const style = {backgroundColor: color };
+        const key = color + Math.random(10);
         elements.push (
-          <div key={colorList[j]} style={style}>color</div>
+          <div key={key} style={style}>color</div>
         )
     }
     
     // Update swatches in state
-    this.setState({ swatches: elements});
+    this.setState({ colorElements: elements});
   }
 
 
   handleKeyPress (e) {
-      if (e.charCode == 13) {
-        alert('Enter... ');
-      }
-      if (e.charCode == 32) {
-        //alert('Spacebar... ');
-        this.updateColors();
-      }
+    const spacebar = 32;
+    if (e.charCode == spacebar) {
+      this.updateColors();
+      this.move();
+    }
   }
   
 
   render() {
-    
+    //const items = this.state.colors;
+    //const items = this.state.items;
+    const items = this.state.colors2;
+
     return (
       <div>
-      {this.state.swatches}
+      {this.state.colorElements}
       <input type="text" id="one" onKeyPress={this.handleKeyPress} />
+
+      <div className='container-fluid text-center ani-show'>
+
+        <StaggeredMotion
+          // Set default style for each to 0
+          defaultStyles={items.map(e=>({translateY:0}))} 
+
+          styles={prevInterpolated => prevInterpolated.map((e,i) => {
+            return i===0
+              ? {translateY: spring(this.state.yOff)}
+              : {translateY: spring(prevInterpolated[i-1].translateY)}
+          })}
+          >
+          {interpolated =>
+            <div className='container-fluid row block-center'>
+              {interpolated.map((style,i) => (
+                 <div key={i}
+                  className='item2 col-6 col-sm-2'
+                  style={
+                    {
+                      transform: 'translateY('+style.translateY+'px)',
+                      backgroundColor: items[i].text
+                    }
+                  }>
+                </div>
+               
+          ))}
+            </div>
+          }
+        </StaggeredMotion>
+
+      </div>
       </div>
     )
   }
@@ -74,3 +143,16 @@ class ColorDisplay extends Component{
 
 export default ColorDisplay;
 
+/*
+ <div key={i}
+                  className='item2 col-6 col-sm-2'
+                  style={
+                    {
+                      transform: 'translateY('+style.translateY+'px)',
+                      backgroundColor: this.state.colors[i]
+                    }
+                  }>
+                </div>
+
+
+*/
